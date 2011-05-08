b01ea534f3f2e0654af7d28ab1c2d2caf626cfb1
Paweł Nadolski <pawel.nadolski@gmail.com>
2011-05-08 14:27:29 +0200

    simplify answer for D
    Changes time complexity from O(n*log(n)) to O(n).

--- 2011/D.py
+++ 2011/D.py
@@ -2,10 +2,9 @@
 for case in xrange(1, int(raw_input())+1):
 	N = int(raw_input())
 	a = map(int, raw_input().split())
-	b = sorted(a)
 	num_unsorted = 0.0
 	for i in xrange(len(a)):
-		if a[i] != b[i]:
+		if a[i] != i+1:
 			num_unsorted += 1
 
 	print("Case #%d: %f" % (case, num_unsorted))
